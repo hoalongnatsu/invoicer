64e08465d8324a18b8f06e8127286b1c7fae93e0
hoalongnatsu <hoalongnatsu@gmail.com>
2022-11-18 16:43:38 +0700

    add support csrf

--- main.go
+++ main.go
@@ -186,6 +186,12 @@ func (iv *invoicer) putInvoice(w http.ResponseWriter, r *http.Request) {
 }
 
 func (iv *invoicer) deleteInvoice(w http.ResponseWriter, r *http.Request) {
+	if !checkCSRFToken(r.Header.Get("X-CSRF-Token")) {
+		w.WriteHeader(http.StatusNotAcceptable)
+		w.Write([]byte("Invalid CSRF Token"))
+		return
+	}
+
 	vars := mux.Vars(r)
 	log.Println("deleting invoice", vars["id"])
 	var i1 Invoice
@@ -242,4 +248,8 @@ func getVersion(w http.ResponseWriter, r *http.Request) {
 "commit": "%s",
 "build": "https://circleci.com/gh/Securing-DevOps/invoicer/"
 }`, version, commit)))
+}
+
+func checkCSRFToken() bool {
+	return true
 }
